b5f1b1c3b8f6539a9f832f73b3c18302b4b99bb2
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2022-10-01 13:44:56 +0530

    progress2

--- botv3.py
+++ botv3.py
@@ -48,7 +48,7 @@ question_3 = questions(message=
                        """)
 question_4 = questions(message=
                        """
-                       Ok Great
+                       Ok Great !
                        How much Commission you are getting on your Platform ?
                        1. 50 % Plus
                        2. 75 % Plus
@@ -60,9 +60,60 @@ question_5 = questions(message=
                        2. 20,000
 
                        """)
+question_6 = questions(message=
+                       """
+                       That's Very less..                       
+                       Do you get total commission, daily in your bank account ?
+                       YES or NO 
+                       """)
+question_7 = questions(message=
+                       """
+                       Do you Know? Your platform is taking advantage of your hard work.
+                       LeadsGuru is giving More than 90% commission to its affiliate with Daily withdrawal.
+                       Are you Excited to start Affiliate  Marketing with LeadsGuru ?
+                       "Type YES"
+                       """)
+question_8 = questions(message=
+                       """
+                       Are you sure 10,000 is greater than 20,000?
+                       """)
+question_9 = questions(message=
+                       """
+                       Well, you know what? I can help you Earn that greater figure of 20,000 within a week? JOKES APART!
+                       Are you ready for that cha-ching sound of money?
+                       Say "yes"
+                       
+                       """)
+question_10 = questions(message=
+                       """
+                       Now! PLEASE NOTE that this is not a get-rich-quick scheme or a magic pill.
+                       However, if you work exactly according to our strategies you will make a very handsome amount. I promise. YES?
+
+                       """)
+question_11 = questions(message=
+                       """
+                       So, Affiliate marketing is an Online business in which anyone can promote or recommend products and earn commissions based on their sales.
+                       """)
+question_12 = questions(message=
+                       """
+                       But why you need to start Affiliate Marketing with LeadsGuru ?
+                       You will get :
+                        -90% Commission
+                        -Same Day Payout
+                        -2 Tier Passive Income
+                        -You DO NOT need to Create Product
+                        -Valuable Courses to Upgrade Skills
+                        Shall we go ahead? Something interesting is waiting for you.YES?
+                       """)
+question_13 = questions(message=
+                       """
+                       LeadsGuru is one of the best Platform That Provides multiple Premium Courses and An Opportunity To Become financial Free by Joining as affiliate.
+                       """)
+
+
 
 def last2nd_message():
-    sleep(1)
+
     lastmessage = driver.find_elements(By.CLASS_NAME, 'message-out')
     message = lastmessage[-1].text
 
@@ -112,12 +163,38 @@ def send_message():
         elif last2nd_message() == "are you already into affiliate marketing/web marketing?" and last_message() == "no":
             question_5.send()
 
-
+        # for answer to be yes----------------------------------------------------------
         elif last2nd_message() == "4. i am not in affiliate marketing business":
             if last_message() == "1" or last_message()== "2" or last_message() == "3":
                 question_4.send()
             if last_message() == "4":
                 question_5.send()
+
+        elif last2nd_message() == "2. 75 % Plus":
+            if last_message() == "1" or last_message()== "2":
+                question_6.send()
+
+
+        elif last2nd_message() == "yes or no":
+            if last_message() == "yes" or last_message()== "no":
+                question_7.send()
+
+        elif last2nd_message() == "type yes" and last_message() == "yes":
+            question_12.send()
+
+        # for answer to be no------------------------------------------------------------
+        elif last2nd_message() == "2. 20,000":
+            if last_message() == "1":
+                question_8.send()
+            if last_message() == "2":
+                question_9.send()
+        elif last2nd_message() == "say yes" and last_message() == "yes":
+            question_10.send()
+        elif last2nd_message() == "however, if you work exactly according to our strategies you will make a very handsome amount. i promise. yes?" and last_message() == "yes":
+            question_11.send()
+            question_12.send()
+
+
 
 
 
@@ -144,8 +221,9 @@ def get_element():
 
     greendot = driver.find_elements(By.CLASS_NAME, "_1pJ9J")
     if (greendot):
-        sleep(1)
+
         greendot[-1].click()
+        sleep(1)
         send_message()
     else:
         sleep(1)
@@ -155,6 +233,7 @@ def get_element():
 
 
 get_element()
+sleep(2)
 
 
 
