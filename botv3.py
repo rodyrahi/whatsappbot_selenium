197bcaddc73bd3639772481bb3a36c4cbe120f57
rodyrahi <45394091+rodyrahi@users.noreply.github.com>
2022-10-02 12:38:01 +0530

    bug fixing

--- botv3.py
+++ botv3.py
@@ -8,7 +8,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import os
 import sys
-sys.setrecursionlimit(10**6)
+
+sys.setrecursionlimit(10 ** 6)
 
 options = Options()
 options.add_argument('--profile-directory=Profile 6')
@@ -21,24 +22,21 @@ driver.get('https://web.whatsapp.com')
 input("enter")
 
 
-
-
-
 class questions():
-    def __init__(self , message):
+    def __init__(self, message):
         self.message = message
 
     def send(self):
         xpath_input = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]'
         input_box = driver.find_elements(By.XPATH, xpath_input)
         input_box[0].send_keys(self.message + Keys.ENTER)
+        sleep(1)
         get_element()
+
     def send_next(self):
         xpath_input = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[2]/div[1]/div/div[1]'
         input_box = driver.find_elements(By.XPATH, xpath_input)
         input_box[0].send_keys(self.message + Keys.ENTER)
-
-
 
 
 question_1 = questions(message=
@@ -86,7 +84,7 @@ question_7 = questions(message=
                        Do you Know? Your platform is taking advantage of your hard work.
                        LeadsGuru is giving More than 90% commission to its affiliate with Daily withdrawal.
                        Are you Excited to start Affiliate  Marketing with LeadsGuru ?
-                       "Type YES"
+                       Type YES
                        """)
 question_8 = questions(message=
                        """
@@ -98,17 +96,17 @@ question_9 = questions(message=
                        Are you ready for that cha-ching sound of money? Say YES
                        """)
 question_10 = questions(message=
-                       """
+                        """
                        Now! PLEASE NOTE that this is not a get-rich-quick scheme or a magic pill.
                        However, if you work exactly according to our strategies you will make a very handsome amount. I promise. YES?
 
                        """)
 question_11 = questions(message=
-                       """
+                        """
                        So, Affiliate marketing is an Online business in which anyone can promote or recommend products and earn commissions based on their sales.
                        """)
 question_12 = questions(message=
-                       """
+                        """
                        But why you need to start Affiliate Marketing with LeadsGuru ?
                        You will get :
                         -90% Commission
@@ -119,52 +117,53 @@ question_12 = questions(message=
                         Shall we go ahead? Something interesting is waiting for you.YES?
                        """)
 question_13 = questions(message=
-                       """
+                        """
                        LeadsGuru is one of the best Platform That Provides multiple Premium Courses and An Opportunity To Become financial Free by Joining as affiliate.
                        1. I would like to know about the courses.
                        2. I want to see Earning Proofs
                        """)
 question_14 = questions(message=
-                       """
+                        """
                        Make a call on 99999999
                        """)
 question_15 = questions(message=
-                       """
+                        """
                        Like to know about the courses.
                        Courses with Pricing
                        Say YES to Schedule call
                        """)
 unrecognised = questions(message=
-                       """
+                         """
                        Please Type "Hey" to get Started!
                        """)
 stop_bot = questions(message=
-                       """
+                     """
                        Bot is stopped
                        """)
 start_bot = questions(message=
-                       """
+                      """
                        bot-started
                        """)
+
 
 def send_image():
     filepath = r'C:\Users\Home\Desktop\kamingo_icon.png'
     image_url = '/html/body/div[1]/div/div/div[4]/div/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/span/div/div/ul/li[1]/button/input'
     attach_url = '//*[@id="main"]/footer/div[1]/div/span[2]/div/div[1]/div[2]/div/div'
     send_button_url = '/html/body/div[1]/div/div/div[2]/div[2]/span/div/span/div/div/div[2]/div/div[2]/div[2]/div/div'
-    attach_box = driver.find_element(By.XPATH , attach_url)
+    attach_box = driver.find_element(By.XPATH, attach_url)
     attach_box.click()
-    image_box = driver.find_element(By.XPATH , image_url)
+    image_box = driver.find_element(By.XPATH, image_url)
     image_box.send_keys(filepath)
     sleep(1)
-    send_button = driver.find_element(By.XPATH , send_button_url)
+    send_button = driver.find_element(By.XPATH, send_button_url)
     send_button.click()
+    sleep(1)
     question_15.send()
 
 
 def last2nd_message():
     lastmessage = driver.find_elements(By.CLASS_NAME, 'message-out')
-
 
     if lastmessage:
         message = lastmessage[-1].text
@@ -187,6 +186,7 @@ def last_message():
 
     return message
 
+
 def end():
     message = [
         "thanks for giving your time :)"
@@ -202,68 +202,83 @@ def send_message():
     if not last_message() == last2nd_message():
         if not funcs.stop:
             if last_message() == "hey":
+                sleep(1)
                 question_1.send()
             elif last2nd_message() == "are you ready ?" and last_message() == "yes":
+                sleep(1)
                 question_2.send()
             elif last2nd_message() == "are you ready ?" and last_message() == "no":
                 end()
 
             elif last2nd_message() == "are you already into affiliate marketing/web marketing?" and last_message() == "yes":
+                sleep(1)
                 question_3.send()
             elif last2nd_message() == "are you already into affiliate marketing/web marketing?" and last_message() == "no":
+                sleep(1)
                 question_5.send()
 
             # for answer to be yes----------------------------------------------------------
             elif last2nd_message() == "4. i am not in affiliate marketing business":
-                if last_message() == "1" or last_message()== "2" or last_message() == "3":
+                if last_message() == "1" or last_message() == "2" or last_message() == "3":
+                    sleep(1)
                     question_4.send()
                 if last_message() == "4":
+                    sleep(1)
                     question_5.send()
 
-            elif last2nd_message() == "2. 75 % Plus":
-                if last_message() == "1" or last_message()== "2":
+            elif last2nd_message() == "2. 75 % plus":
+                if last_message() == "1" or last_message() == "2":
+                    sleep(1)
                     question_6.send()
 
-
             elif last2nd_message() == "yes or no":
-                if last_message() == "yes" or last_message()== "no":
+                if last_message() == "yes" or last_message() == "no":
+                    sleep(1)
                     question_7.send()
 
             elif last2nd_message() == "type yes" and last_message() == "yes":
+                sleep(1)
                 question_12.send()
 
             # for answer to be no------------------------------------------------------------
             elif last2nd_message() == "2. 20,000":
                 if last_message() == "1":
+                    sleep(1)
                     question_8.send_next()
                     question_9.send()
                 if last_message() == "2":
+                    sleep(1)
                     question_9.send()
             elif last2nd_message() == "are you ready for that cha-ching sound of money? say yes" and last_message() == "yes":
+                sleep(1)
                 question_10.send()
             elif last2nd_message() == "however, if you work exactly according to our strategies you will make a very handsome amount. i promise. yes?" and last_message() == "yes":
+                sleep(1)
                 question_11.send_next()
                 question_12.send()
             elif last2nd_message() == "shall we go ahead? something interesting is waiting for you.yes?" and last_message() == "yes":
+                sleep(1)
                 question_13.send()
             elif last2nd_message() == "2. i want to see earning proofs":
                 if last_message() == "1":
+                    sleep(1)
                     question_14.send()
                 if last_message() == "2":
+                    sleep(1)
                     send_image()
             elif last2nd_message() == "courses with pricing" and last_message() == "yes" and last_message() == "yes":
+                sleep(1)
+                question_14.send()
+            elif last2nd_message() == "say yes to schedule call" and last_message() == "yes":
                 question_14.send()
 
 
-            # bot stop ----------------------------------------------------------------------------------
+# bot stop ----------------------------------------------------------------------------------
             elif last_message() == "admin-stop":
                 stop_bot.send_next()
                 funcs.stop = True
-
-
             else:
                 unrecognised.send()
-
                 sleep(1)
                 get_element()
             sleep(1)
@@ -280,33 +295,19 @@ def send_message():
         get_element()
 
 
-
-
-
-
-
-
 def get_element():
-
     # finding green dot
-
+    sleep(1)
     greendot = driver.find_elements(By.CLASS_NAME, "_1pJ9J")
-    if (greendot):
 
+    if (greendot):
         greendot[-1].click()
         sleep(1)
-
         send_message()
     else:
         sleep(1)
-
         send_message()
-
-
 
 
 get_element()
 sleep(2)
-
-
-
